87a430cda637c2884701b4d5e99deec3146144a7
Kevin <kevintun1122@gmail.com>
2018-06-22 18:50:30 -0700

    - Refactored to use promisify - Total time: ~23 mins to generate and load

---
+++ insertData.sql
@@ -0,0 +1,4 @@
+COPY users FROM '/Users/Kevin/Documents/HackReactor githubs/Capstone/SDC/fakeDataGenerator/data/users.txt';
+COPY photos FROM '/Users/Kevin/Documents/HackReactor githubs/Capstone/SDC/fakeDataGenerator/data/photos.txt';
+COPY restaurants FROM '/Users/Kevin/Documents/HackReactor githubs/Capstone/SDC/fakeDataGenerator/data/restaurants.txt';
+COPY reviews FROM '/Users/Kevin/Documents/HackReactor githubs/Capstone/SDC/fakeDataGenerator/data/reviews.txt';
